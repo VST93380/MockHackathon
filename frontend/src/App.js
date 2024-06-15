92956bfc85b3f517a15876b53167a5afcd722b64
SYAMALA LIKITH SAI REDDY <2100031023cseh@gmail.com>
2024-06-15 16:09:17 +0530

    updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Admin from './components/Admin';
+import User from './components/User';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Doctor from './components/Doctor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/admin" element={<Admin/>} />
+          <Route path="/user" element={<User/>} />
+          <Route path="/doctor" element={<Doctor/>} />
+        </Routes>
+    </Router> 
     </div>
   );
 }
